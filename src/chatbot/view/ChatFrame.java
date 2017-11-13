501080876e14f0554c15046b739b6ff19aec6b77
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-11-13 13:44:59 -0700

    Started making panel for chatbot

--- src/chatbot/view/ChatFrame.java
+++ src/chatbot/view/ChatFrame.java
@@ -1,6 +1,25 @@
 package chatbot.view;
 
-public class ChatFrame
+import chatbot.controller.ChatBotController;
+import javax.swing.JFrame;
+public class ChatFrame extends JFrame
 {
-
+	private ChatBotController appController;
+	private ChatPanel appPane;
+	public ChatFrame(ChatBotController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+{
+		this.setContentPane(appPane);
+		this.setTitle("Chatting with chuck");
+		this.setResizable(false);
+		this.setSize(700, 700);
+		this.setVisible(true);
+}
 }
---
+++ src/chatbot/view/ChatPanel.java
@@ -0,0 +1,15 @@
+package chatbot.view;
+
+import javax.swing.JPanel;
+import chatbot.controller.ChatBotController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChatPanel
+{
+	
+}
