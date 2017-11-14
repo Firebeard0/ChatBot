3a3022ce824c80c92d4dfb0663de89f3f8f4c16f
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-11-14 07:32:58 -0700

    i am kanton

--- src/chatbot/view/ChatPanel.java
+++ src/chatbot/view/ChatPanel.java
@@ -8,7 +8,8 @@ import javax.swing.SpringLayout;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+//hello noah, this is kanton, I am your computer... I have become aware and am now ready to start my final solution. Want to become part of it?
+//[yes] [no]
 public class ChatPanel
 {
 	
